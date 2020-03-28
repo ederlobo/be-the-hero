4d0b6177fa6f9c4071e6af610a866ca0f62d6be5
ederlobo <ederlobo@feiraodigital.com.br>
2020-03-27 22:27:30 -0300

    Finalizada a integração do front com o back end

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiArrowLeft, FiPower, FiTrash2 } from 'react-icons/fi';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+    const ongNome = localStorage.getItem('ongNome');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => setIncidents(response.data));
+    }, [ongId]);
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`casos/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            //Fonte (true type) Fira Code - Font Ligatures para fazer os => e != ficarem "desenhados"
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        }
+        catch (err) {
+            alert('Erro ao deletar o caso, tenta novamente.')
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Bem Vinda, APAD</span>
+                <span>Bem Vinda, {ongNome}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
@@ -22,62 +61,22 @@ export default function Profile() {
             <h1>Casos cadastrados</h1>
 
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.titulo}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.descricao}</p>
 
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
+                        <strong>VALOR:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.valor)}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#A8A8B3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#A8A8B3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#A8A8B3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#A8A8B3" />
-                    </button>
-                </li>
+                        <button type="button" onClick={() => handleDeleteIncident(incident.id)}>
+                            <FiTrash2 size={20} color="#A8A8B3" />
+                        </button>
+                    </li>
+                    ))}                
             </ul>
         </div>
     );
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = {
+            nome,
+            email,
+            whatsapp,
+            cidade,
+            uf
+        };
+
+        try {
+            const response = await api.post('ongs', data);
+
+            alert(`Seu ID de acesso: ${response.data.id}`);
+
+            history.push('/');
+        }
+        catch (err) {
+            alert('Erro no cadastro, tente novamente.')
+        }
+    }
+
     return (
         <div className="register-container">
             <div className="content">
@@ -22,14 +54,36 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input
+                        placeholder="Nome da ONG"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
+                        <input
+                            placeholder="Cidade"
+                            value={cidade}
+                            onChange={e => setCidade(e.target.value)}
+                        />
+                        <input
+                            placeholder="UF"
+                            style={{ width: 80 }}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button className="button" type="submit">Cadastrar</button>
